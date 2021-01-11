b485e8371484bdf8329499d18092dc224b2e3c3e
oskarolaszczyk <oskarolaszczyk@gmail.com>
2021-01-11 21:35:58 +0100

    improved terminal prints, added info and debug logs

--- logger.py
+++ logger.py
@@ -2,10 +2,13 @@ import logging
 
 from data import config_file
 
+
 def init_logger(level):
     path = config_file.directory + "/" + "chase.log"
-    logging.basicConfig(level=level, filename=path, filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+    logging.basicConfig(level=level, filename=path, filemode='w',
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
     logging.info('Init logger')
+
 
 def get_logger():
     return logging.getLogger(__name__)
--- main.py
+++ main.py
@@ -10,42 +10,65 @@ from data import config_file
 from colorama import Fore, Style
 
 
-
 def main():
+    log = f"Game config:  rounds: {config_file.rounds_no}, sheeps_no: {config_file.sheeps_no}, " \
+          f"init_pos_limit: {config_file.init_pos_limit}, sheep_move_dist: {config_file.sheep_move_dist}, " \
+          f"wolf_move_dist {config_file.wolf_move_dist}"
+    logger.get_logger().debug(log)
 
-    # init sheeps and wolf
+    wolf, sheeps = init_animals()
+    simulation(wolf, sheeps, config_file.rounds_no)
+
+
+# ----------OTHER FUNCTIONS-----------
+
+
+def init_animals():
     wolf = Wolf(config_file.wolf_move_dist)
     sheeps = []
 
     for i in range(config_file.sheeps_no):
-        sheep = Sheep(config_file.sheep_move_dist, config_file.init_pos_limit, i+1)
+        sheep = Sheep(config_file.sheep_move_dist, config_file.init_pos_limit, i + 1)
         sheeps.append(sheep)
 
-    simulation(wolf, sheeps, config_file.rounds_no)
+    log = f"Animals:  wolf: {wolf}, sheeps: {sheeps}"
+    logger.get_logger().debug(log)
+
+    return wolf, sheeps
 
 
 def simulation(wolf, sheeps, rounds):
     for i in range(rounds):
         if get_dies_count(sheeps) == config_file.sheeps_no:
             break
-        print("round_no: " + str(i + 1))
+        round_log = f"round_no: {i+1}"
+        print(Fore.CYAN + round_log + Style.RESET_ALL)
         for sheep in sheeps:
             sheep.move()
         min_distance, nearest_sheep = find_nearest_sheep(wolf, sheeps)
+        start_wolf_position = [round(pos, 3) for pos in wolf.position]
         killed_sheep_index = wolf.try_catch_sheep(nearest_sheep, min_distance)
         # json export
-        json_data = create_json(sheeps, wolf, i+1)
-        write_json(i+1, json_data)
-        #csv export
-        write_csv(i+1, get_alive_count(sheeps))
-        #terminal info
+        json_data = create_json(sheeps, wolf, i + 1)
+        write_json(i + 1, json_data)
+        # csv export
+        write_csv(i + 1, get_alive_count(sheeps))
+
+        rounded_wolf_position = [round(pos, 3) for pos in wolf.position]
+        other_info_log = f"wolf start position: {rounded_wolf_position}\nalive: {get_alive_count(sheeps)}\ndied: {get_dies_count(sheeps)}\nwolf end position{start_wolf_position}\n"
+        # terminal info
         if killed_sheep_index is not None:
-            print(Fore.RED + "sheep died: " + str(killed_sheep_index), end='')
-            print(Style.RESET_ALL)
-        print("wolf position" + str(wolf.position) + "\nalive:" + str(get_alive_count(sheeps)) + "\ndied:" + str(get_dies_count(sheeps)) + "\n")
+            killed_sheep_index_log = f"sheep died: {killed_sheep_index}\n"
+            other_info_log += killed_sheep_index_log
+            # print(Fore.RED + killed_sheep_index_log, end='')
+
+        print(other_info_log.replace("sheep died", Fore.RED + "sheep died"))
+
+        logger.get_logger().info(round_log + ", " + other_info_log[:-1].replace("\n", ", "))
         if config_file.wait:
-            input(Fore.GREEN+"Press enter to continue symulation")
-            print(Style.RESET_ALL)
+            input(Fore.GREEN + "Press enter to continue symulation")
+        print(Style.RESET_ALL)
+
 
 def find_nearest_sheep(wolf, sheeps):
     min_distance = config_file.init_pos_limit + 1000
@@ -56,6 +79,9 @@ def find_nearest_sheep(wolf, sheeps):
             if dist < min_distance:
                 min_distance = dist
                 nearest_sheep = sheep
+
+    log = f"min_distance: {min_distance}, nearest_sheep: {nearest_sheep}"
+    logger.get_logger().debug(log)
     return min_distance, nearest_sheep
 
 
@@ -65,7 +91,11 @@ def get_dies_count(sheeps):
         if not sheep.alive:
             count += 1
 
+    log = f"dies_count: {count}"
+    logger.get_logger().debug(log)
+
     return count
+
 
 def get_alive_count(sheeps):
     count = 0
@@ -73,7 +103,11 @@ def get_alive_count(sheeps):
         if sheep.alive:
             count += 1
 
+    log = f"alive_count: {count}"
+    logger.get_logger().debug(log)
+
     return count
+
 
 def create_json(sheeps, wolf, round_no):
     x = {
@@ -88,12 +122,15 @@ def create_json(sheeps, wolf, round_no):
                 "sheep_id": sheep.id_number,
                 "sheep_pos": {"x": sheep.get_x(),
                               "y": sheep.get_y()}
-                })
+            })
         else:
             sheeps_pos.append({
                 "sheep_id": sheep.id_number,
                 "sheep_pos": None})
     x['sheeps_pos'] = sheeps_pos
+
+    log = f"created_json: {x}"
+    logger.get_logger().debug(log)
 
     return x
 
@@ -104,9 +141,15 @@ def write_json(round_no, data, filename='pos.json'):
     if round_no == 1:
         with open(path, 'w') as f:
             f.write(json.dumps(data, indent=5))
+
+        log = f"json created"
+        logger.get_logger().debug(log)
     else:
         with open(path, 'a') as f:
             f.write(json.dumps(data, indent=5))
+
+        log = f"json updated"
+        logger.get_logger().debug(log)
 
 
 def write_csv(round_no, alive_count, filename='alive.csv'):
@@ -118,6 +161,9 @@ def write_csv(round_no, alive_count, filename='alive.csv'):
 
             writer.writeheader()
             writer.writerow({'round_no': round_no, 'alive': alive_count})
+
+        log = f"csv created"
+        logger.get_logger().debug(log)
     else:
         with open(path, mode='a') as csv_file:
             fieldnames = ['round_no', 'alive']
@@ -125,6 +171,8 @@ def write_csv(round_no, alive_count, filename='alive.csv'):
 
             writer.writerow({'round_no': round_no, 'alive': alive_count})
 
+        log = f"csv updated"
+        logger.get_logger().debug(log)
 
 
 if __name__ == "__main__":
